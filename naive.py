40ebe4960a3c04e523c6001094a8bfb29fe40abd
Micheal Oyenekan <60825726+moyeneka@users.noreply.github.com>
2021-12-02 22:19:17 -0600

    good

--- naive.py
+++ naive.py
@@ -1,10 +1,11 @@
 from typing import Counter
 import pandas as pd
-from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from matplotlib import pyplot as plt
+from sklearn.svm import SVC
+from sklearn.metrics import RocCurveDisplay
 
 # read the train and test dataset
 train_Test_data = pd.read_csv('spam_ham_dataset.csv')
@@ -24,7 +25,19 @@ train_count_X.toarray()[:3]
 model_Multi= MultinomialNB()
 model_Multi.fit(train_count_X,train_Y)
 
+test_count_X = vec.transform(test_X)
+print("score Test set = ",model_Multi.score(train_count_X,train_Y))
 
+print("score Training set = ",model_Multi.score(test_count_X,test_Y))
+
+svc_disp = RocCurveDisplay.from_estimator(model_Multi, test_count_X, test_Y)
+ax = plt.gca()
+rfc_disp = RocCurveDisplay.from_estimator(model_Multi, test_count_X, test_Y, ax=ax, alpha=0.8)
+svc_disp.plot(ax=ax, alpha=0.8)
+plt.show()
+#plt.plot(test_count_X,test_Y)
+#X train = text
+#test set = spam or not
 
 
 
